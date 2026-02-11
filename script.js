e22910e2c48b9ebca41c3b22316ca3d265d311e1
sreehariar04 <sreehariar04@gmail.com>
2026-02-11 11:05:11 +0530

    Initial commit - Calculator application

---
+++ script.js
@@ -0,0 +1,17 @@
+function appendValue(value) {
+  document.getElementById('result').value += value;
+}
+
+function clearResult() {
+  document.getElementById('result').value = '';
+}
+
+function calculateResult() {
+  var result = document.getElementById('result').value;
+  try {
+    var calculatedResult = eval(result);
+    document.getElementById('result').value = calculatedResult;
+  } catch (error) {
+    document.getElementById('result').value = 'Error';
+  }
+}
